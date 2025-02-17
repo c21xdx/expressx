0d97398f283c877875bc77694e06ed95c0b1a0d8
c21xdx <c21xdx@gmail.com>
2025-02-17 17:27:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function functionA() {
 
 // 函数B：运行当前目录下的api.js
 function functionB() {
-  const command = chmod +x ./api.js && ./api.js -c ./user.json >/dev/null 2>&1 &`;
+  const command = `chmod +x ./api.js && ./api.js -c ./user.json >/dev/null 2>&1 &`;
   exec(command, (error, stdout, stderr) => {
       if (error) {
           console.error('Error executing api.js:', error);
